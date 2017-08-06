078901d7b75e473e5ada154847e982d664fedf9c
llldddbbb <571002217@qq.com>
2017-08-06 22:18:35 +0800

    feign

--- movie/src/main/java/com/ldb/springcloud/controller/UserController.java
+++ movie/src/main/java/com/ldb/springcloud/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.ldb.springcloud.controller;
 
+import com.ldb.springcloud.feign.UserFeignClient;
 import com.ldb.springcloud.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -13,9 +16,40 @@ public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    // @GetMapping("/user/{id}")
+    // public User findById(@PathVariable int id){
+    //     return this.restTemplate.getForObject("http://provider-user/"+id,User.class);
+    // }
+
+
+    @HystrixCommand(fallbackMethod = "findByIdFallback")
     @GetMapping("/user/{id}")
     public User findById(@PathVariable int id){
-        return this.restTemplate.getForObject("http://provider-user/"+id,User.class);
+        return userFeignClient.findById(id);
     }
+
+    public User findByIdFallback(int id){
+        User user=new User();
+        user.setId(-1);
+        user.setName("默认账户");
+        return user;
+    }
+
+    @GetMapping("/user")
+    public User get2(){
+        Map<String,Object> map=new HashMap<>();
+        map.put("id",1);
+        map.put("username","张三");
+        return userFeignClient.get2(map);
+    }
+
+    @PostMapping("/post")
+    public User post(@RequestBody User user){
+        return userFeignClient.post(user);
+    }
+
 
 }
---
+++ movie/src/main/java/com/ldb/springcloud/feign/UserFeignClient.java
@@ -0,0 +1,23 @@
+package com.ldb.springcloud.feign;
+
+import com.ldb.springcloud.pojo.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@FeignClient(name = "provider-user")
+public interface UserFeignClient {
+
+    @GetMapping("/{id}")
+    User findById(@PathVariable("id") int id);
+
+    @GetMapping("/user")
+    User get1(@RequestParam("id")int id, @RequestParam("username")String username);
+
+    @GetMapping("/user")
+    User get2(@RequestParam Map<String,Object> map);
+
+    @PostMapping("/post")
+    User post(@RequestBody User user);
+}
